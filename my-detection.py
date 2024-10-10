4165a141a06d48b9e57f9bf859f60abe30574f2a
noob58486 <162982670+noob58486@users.noreply.github.com>
2024-10-10 21:13:41 +0800

    Add files via upload

--- my-detection.py
+++ my-detection.py
@@ -2,15 +2,19 @@ import jetson.inference
 import jetson.utils
 
 net=jetson.inference.detectNet("ssd-mobilenet-v2",threshold=0.5)
-camera=jetson.utils.videoSource("/home/nvidia/jetson-inference/data/images/airplane_0.jpg")
+camera=jetson.utils.videoSource("/home/nvidia/jetson-inference/data/images/object_6.jpg")
 display=jetson.utils.videoOutput("display://0")
-while display.IsStreaming():
-    img=camera.Capture()
-    if img is None:
-        continue
+img=camera.Capture()
+detections=net.Detect(img)
+print(detections[0])
+display.Render(img)
+display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS))
+#while display.IsStreaming():
+ #   img=camera.Capture()
+  #  if img is None:
+   #     continue
 
-    detections=net.Detect(img)
-    print(detections[0])
-    display.Render(img)
-    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
-
+    #detections=net.Detect(img)
+    #print(detections[0])
+    #display.Render(img)
+    #display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS))
